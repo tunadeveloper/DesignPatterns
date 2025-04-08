bc805d6db62ee91fde7a9414839fe64db3da5bee
tunadeveloper <tunabusiness25@gmail.com>
2025-04-08 23:37:32 +0300

    Updated

---
+++ ObserverDesignPattern/DesignPattern.Observer/DataAccessLayer/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Observer.DAL
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=TUNA\\SQLEXPRESS;initial catalog=Observer;integrated security=true;");
+        }
+        public DbSet<WelcomeMessage> WelcomeMessages { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<UserProcess> UserProcesses { get; set; }
+    }
+}
---
+++ ObserverDesignPattern/DesignPattern.Observer/Migrations/20250408201714_mig1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DesignPattern.Observer.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_welcomeMessages",
+                table: "welcomeMessages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_userProcesses",
+                table: "userProcesses");
+
+            migrationBuilder.RenameTable(
+                name: "welcomeMessages",
+                newName: "WelcomeMessages");
+
+            migrationBuilder.RenameTable(
+                name: "userProcesses",
+                newName: "UserProcesses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_WelcomeMessages",
+                table: "WelcomeMessages",
+                column: "WelcomeMessageID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserProcesses",
+                table: "UserProcesses",
+                column: "UserProcessID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_WelcomeMessages",
+                table: "WelcomeMessages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserProcesses",
+                table: "UserProcesses");
+
+            migrationBuilder.RenameTable(
+                name: "WelcomeMessages",
+                newName: "welcomeMessages");
+
+            migrationBuilder.RenameTable(
+                name: "UserProcesses",
+                newName: "userProcesses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_welcomeMessages",
+                table: "welcomeMessages",
+                column: "WelcomeMessageID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_userProcesses",
+                table: "userProcesses",
+                column: "UserProcessID");
+        }
+    }
+}
